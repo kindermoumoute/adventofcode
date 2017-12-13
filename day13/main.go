c970a3f811f58ff81d8e3366f4ae1818564de12f
kindermoumoute <kindermoumoute@gmail.com>
2017-12-13 21:57:39 +0100

    Day 13
    Quickest solution*

---
+++ day13/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	part1, picosec, severity := 0, 0, 1
+
+	list := parse(puzzle)
+
+	// part 1
+	for _, l := range list {
+		ss := sevScore(picosec, l.depth, l.size)
+		if ss == 0 {
+			part1 += l.depth * l.size
+		}
+	}
+	// part 2
+	for ; severity != 0; picosec++ {
+		severity = 0
+		for _, l := range list {
+			ss := sevScore(picosec, l.depth, l.size)
+			if ss == 0 {
+				severity += l.depth*l.size + 1
+				break
+			}
+		}
+	}
+	fmt.Println(part1, picosec-1)
+}
+func sevScore(p, d, s int) int {
+	return (p + d) % ((s - 1) * 2)
+}
+
+type layer struct {
+	size, depth int
+}
+
+func parse(s string) []layer {
+	lines := strings.Split(s, "\n")
+	list := make([]layer, len(lines))
+	for i, line := range lines {
+		linesSplitted := strings.Split(line, ": ")
+		list[i].depth, _ = strconv.Atoi(linesSplitted[0])
+		list[i].size, _ = strconv.Atoi(linesSplitted[1])
+	}
+	return list
+}
