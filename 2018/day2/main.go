30c2b5b9381caa3f9303308a8aaa3327b09c84c2
Kindermoumoute <kindermoumoute@gmail.com>
2018-12-02 06:27:38 +0100

    Day 2

--- 2018/day2/main.go
+++ 2018/day2/main.go
@@ -2,26 +2,73 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 // returns part1 and part2
 func run(input string) (string, string) {
-	//list := parse(input)
-	//intList := pkg.ParseIntList(input)
+	part1, part2 := parse(input)
 
-	// fmt.Println("PASS")
-	return "", ""
+	return strconv.Itoa(part1), part2
 }
 
-//func parse(s string) {
-//	lines := strings.Split(s, "\n")
-//	for i, line := range lines {
-//
-//	}
-//	return
-//}
+func parse(s string) (int, string) {
+	lines := strings.Split(s, "\n")
+
+	// part1
+	mapLineChar := make(map[string]map[int32]int)
+	twoTimes := 0
+	threeTimes := 0
+	for _, line := range lines {
+		mapLineChar[line] = make(map[int32]int)
+		for _, char := range line {
+			mapLineChar[line][char]++
+		}
+		countTwoExist, countThreeExist := false, false
+		for _, count := range mapLineChar[line] {
+			if !countTwoExist && count == 2 {
+				countTwoExist = true
+				twoTimes++
+			}
+			if !countTwoExist && count == 2 {
+				countTwoExist = true
+				twoTimes++
+			}
+			if !countThreeExist && count == 3 {
+				countThreeExist = true
+				threeTimes++
+			}
+			if countTwoExist && countThreeExist {
+				break
+			}
+		}
+	}
+
+	//part2
+	commonLetters := ""
+dance:
+	for _, line := range lines {
+		for _, line2 := range lines {
+			miss := -1
+			for i := range line {
+				if line[i] != line2[i] {
+					if miss != -1 {
+						miss = -1
+						break
+					}
+					miss = i
+				}
+			}
+			if miss != -1 {
+				commonLetters = line[:miss] + line[miss+1:]
+				break dance
+			}
+		}
+	}
+	return twoTimes * threeTimes, commonLetters
+}
 
 func main() {
-	tests.Run(run, false)
 	fmt.Println(run(puzzle))
 }
