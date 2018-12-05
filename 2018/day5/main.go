7686caeaf6001f36064f10072ab820641b27616d
Kindermoumoute <kindermoumoute@gmail.com>
2018-12-05 23:29:04 +0100

    Optimize and refacto day5

--- 2018/day5/main.go
+++ 2018/day5/main.go
@@ -1,83 +1,52 @@
 package main
 
 import (
-	"fmt"
 	"strconv"
-	"strings"
 
 	"github.com/kindermoumoute/adventofcode/pkg"
 )
 
-const toUp = 'a' - 'A'
-const toDown = 'A' - 'a'
-
 // returns part1 and part2
 func run(input string) (string, string) {
-	part1 := []byte(input)
-	//list := parse(input)
-	//intList := pkg.ParseIntList(input)
-	iterator := 0
-	for {
-		if iterator+1 > len(part1)-1 {
-			break
-		}
-		if iterator < 0 {
-			iterator++
-		}
-		diff := part1[iterator+1] - part1[iterator]
-		if diff == 32 || diff == 224 {
-			part1 = append(part1[:iterator], part1[iterator+2:]...)
-			iterator -= 2
-		}
-		iterator++
-	}
-	all := make(map[byte][]byte)
-	minI := byte(0)
-	min := 9999999999999
-	for i := byte('a'); i < 'z'; i++ {
-		tmp := string([]byte{i})
-		tmpUP := string([]byte{i - 32})
-		fmt.Println("LETTERS ", tmp, tmpUP)
-		all[i] = []byte(strings.Replace(strings.Replace(input, tmpUP, "", -1), tmp, "", -1))
-		fmt.Println(string(all[i]))
-		iterator := 0
-		for {
-			if iterator+1 > len(all[i])-1 {
-				break
-			}
-			if iterator < 0 {
-				iterator++
-			}
-			diff := all[i][iterator+1] - all[i][iterator]
-			if diff == 32 || diff == 224 {
-				all[i] = append(all[i][:iterator], all[i][iterator+2:]...)
-				iterator -= 2
-			}
-			iterator++
-		}
+	// part1
+	part1 := Polymer(input).react()
 
-		fmt.Println(string(all[i]))
-		if len(all[i]) < min {
-			min = len(all[i])
-			minI = i
+	// part2
+	polymers, minLetter, min := make(map[byte]Polymer), byte(0), 9999999999999
+	for letter := byte('a'); letter < 'z'; letter++ {
+		polymers[letter] = removeLetterFromPolymer(Polymer(input), letter).react()
+		if len(polymers[letter]) < min {
+			min = len(polymers[letter])
+			minLetter = letter
 		}
 	}
-	return strconv.Itoa(len(part1)), strconv.Itoa(len(all[minI]))
+	return strconv.Itoa(len(part1)), strconv.Itoa(len(polymers[minLetter]))
 }
 
-func parse(s string) {
-	lines := strings.Split(s, "\n")
-	for _, line := range lines {
-		n, err := fmt.Sscanf(line, "")
-		pkg.Check(err)
-		if n != 0 {
-			panic(fmt.Errorf("%d args expected in scanf, got %d", 0, n))
+type Polymer []byte
+
+func (p Polymer) react() Polymer {
+	for i := 0; i < len(p)-1; i++ {
+		if p[i+1]^p[i] == 32 {
+			p = append(p[:i], p[i+2:]...)
+			if i -= 2; i < -1 {
+				i = -1
+			}
 		}
 	}
-	return
+	return p
+}
+
+func removeLetterFromPolymer(p Polymer, letter byte) Polymer {
+	for i := 0; i < len(p); i++ {
+		if letter == p[i] || letter-32 == p[i] {
+			p = append(p[:i], p[i+1:]...)
+			i--
+		}
+	}
+	return p
 }
 
 func main() {
-	tests.Run(run, false)
-	fmt.Println(run(puzzle))
+	pkg.Execute(run, tests, puzzle, false)
 }
