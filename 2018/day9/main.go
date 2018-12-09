2b487d32447c7399918f81e3c4eaae56de446b3c
Kindermoumoute <kindermoumoute@gmail.com>
2018-12-09 09:24:51 +0100

    Day9

--- 2018/day9/main.go
+++ 2018/day9/main.go
@@ -1,31 +1,68 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
-	"strings"
+	"sort"
+	"strconv"
 
 	"github.com/kindermoumoute/adventofcode/pkg"
 )
 
 // returns part1 and part2
 func run(input string) (string, string) {
-	//list := parse(input)
-	//intList := pkg.ParseIntList(input)
+	players, lastMarble := parse(input)
+	scores := make([]int, players)
+	game := list.New()
+	currentMarble := game.PushFront(0)
+	currentPlayer := 0
+	actualLastMarble := lastMarble * 100
+	part1 := 0
+	for i := 1; i <= actualLastMarble; i++ {
+		if i%23 == 0 && i > 0 {
+			scores[currentPlayer] += i
+			for j := 0; j < 7; j++ {
+				currentMarble = currentMarble.Prev()
+				if currentMarble == nil {
+					currentMarble = game.Back()
+				}
+			}
+			toRemove := currentMarble
+			scores[currentPlayer] += currentMarble.Value.(int)
+			currentMarble = currentMarble.Next()
+			if currentMarble == nil {
+				currentMarble = game.Front()
+			}
+			game.Remove(toRemove)
 
-	// fmt.Println("PASS")
-	return "", ""
+		} else {
+			currentMarble = currentMarble.Next()
+			if currentMarble == nil {
+				currentMarble = game.Front()
+			}
+			currentMarble = game.InsertAfter(i, currentMarble)
+		}
+		if i%lastMarble == 0 && i/lastMarble == 1 {
+			tmpScores := make([]int, players)
+			copy(tmpScores, scores)
+			sort.Ints(tmpScores)
+			part1 = tmpScores[len(tmpScores)-1]
+		}
+		currentPlayer = (currentPlayer + 1) % players
+
+	}
+	sort.Ints(scores)
+	return strconv.Itoa(part1), strconv.Itoa(scores[len(scores)-1])
 }
 
-func parse(s string) {
-	lines := strings.Split(s, "\n")
-	for _, line := range lines {
-		n, err := fmt.Sscanf(line, "")
-		pkg.Check(err)
-		if n != 0 {
-			panic(fmt.Errorf("%d args expected in scanf, got %d", 0, n))
-		}
+func parse(s string) (int, int) {
+	var players, lastMarble int
+	n, err := fmt.Sscanf(s, "%d players; last marble is worth %d points", &players, &lastMarble)
+	pkg.Check(err)
+	if n != 2 {
+		panic("err")
 	}
-	return
+	return players, lastMarble
 }
 
 func main() {
--- 2018/day9/puzzle.go
+++ 2018/day9/puzzle.go
@@ -4,10 +4,30 @@ import "github.com/kindermoumoute/adventofcode/pkg"
 
 var tests = pkg.TestCases{
 	{
-		``,
-		``,
-		``,
+		`10 players; last marble is worth 25 points`,
+		`32`,
+		`19708`,
+	},
+	{
+		`10 players; last marble is worth 1618 points`,
+		`8317`,
+		`74765078`,
+	},
+	{
+		`13 players; last marble is worth 7999 points`,
+		`146373`,
+		`1406506154`,
+	},
+	{
+		`17 players; last marble is worth 1104 points`,
+		`2764`,
+		`20548882`,
+	},
+	{
+		puzzle,
+		`422748`,
+		`3412522480`,
 	},
 }
 
-var puzzle = ``
+var puzzle = `430 players; last marble is worth 71588 points`
