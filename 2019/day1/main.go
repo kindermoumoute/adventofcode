24f766b409e7c8fc96ba4fce9358719fff3d4ebf
Kindermoumoute <kindermoumoute@gmail.com>
2019-12-01 06:14:17 +0100

    feat(2019): day1

--- 2019/day1/main.go
+++ 2019/day1/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"strings"
 
 	"github.com/kindermoumoute/adventofcode/pkg"
@@ -8,19 +9,31 @@ import (
 
 // returns part1 and part2
 func run(input string) (string, string) {
-	//list := parse(input)
+	part1, part2 := parse(input)
 	//intList := pkg.ParseIntList(input)
 	// bufio.NewReader(os.Stdin).ReadBytes('\n')
 	//strconv.Itoa()
-	return "", ""
+	return strconv.Itoa(part1), strconv.Itoa(part2)
 }
 
-func parse(s string) {
+func parse(s string) (int, int) {
 	lines := strings.Split(s, "\n")
+	part1 := 0
+	part2 := 0
 	for _, line := range lines {
-		pkg.MustScanf(line, "")
+		i, err := strconv.Atoi(line)
+		pkg.Check(err)
+		part1 += i/3 - 2
+		part2 += sumRec(i)
 	}
-	return
+	return part1, part2
+}
+func sumRec(i int) int {
+	i = i/3 - 2
+	if i <= 0 {
+		return 0
+	}
+	return (i) + sumRec(i)
 }
 
 func main() {
