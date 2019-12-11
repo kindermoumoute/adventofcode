7d9a0d5b30885d1b51413d9e49523c1b8bf3a73c
Kindermoumoute <kindermoumoute@gmail.com>
2019-12-11 11:27:11 +0100

    chore: user interface for expected tests

--- pkg/test.go
+++ pkg/test.go
@@ -8,26 +8,27 @@ import (
 type TestCases []TestCase
 
 type TestCase struct {
-	Input, ExpectedPart1, ExpectedPart2 string
+	Input                        string
+	ExpectedPart1, ExpectedPart2 interface{}
 }
 
 func (t TestCases) Run(fn func(string) (interface{}, interface{}), hideInput bool) {
 	for _, test := range t {
 		part1I, part2I := fn(test.Input)
-		part1, part2 := fmt.Sprint(part1I), fmt.Sprint(part2I)
-		passedPart1 := part1 == test.ExpectedPart1 || test.ExpectedPart1 == ""
-		passedPart2 := part2 == test.ExpectedPart2 || test.ExpectedPart2 == ""
+		part1, expectedPart1, part2, expectedPart2 := fmt.Sprint(part1I), fmt.Sprint(test.ExpectedPart1), fmt.Sprint(part2I), fmt.Sprint(test.ExpectedPart2)
+		passedPart1 := part1 == expectedPart1 || expectedPart1 == ""
+		passedPart2 := part2 == expectedPart2 || expectedPart2 == ""
 		passed := passedPart1 && passedPart2
 
 		if !passed && !hideInput {
 			fmt.Println("Input ", test.Input)
 		}
 		if !passedPart1 {
-			fmt.Println(" - PART1: ", part1, " but expected ", test.ExpectedPart1)
+			fmt.Println(" - PART1: ", part1, " but expected ", expectedPart1)
 			os.Exit(1)
 		}
 		if !passedPart2 {
-			fmt.Println(" - PART2: ", part2, " but expected ", test.ExpectedPart2)
+			fmt.Println(" - PART2: ", part2, " but expected ", expectedPart2)
 			os.Exit(1)
 		}
 	}
