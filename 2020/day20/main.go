6c7dd929df99cdfb5e03923149ec28175c05f1dd
Olivier CANO <ocano@scaleway.com>
2020-12-20 15:02:07 +0100

    chore(2020): clean up day20

--- 2020/day20/main.go
+++ 2020/day20/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"image/color"
 	"strings"
 
@@ -17,195 +16,73 @@ func run(input string) (interface{}, interface{}) {
 		'#': colornames.Black,
 		'O': colornames.Red,
 	}
-	twod.SetFPS(10)
-	tiles := parse(input)
+	twod.SetFPS(60)
+	bordersPerTileID, tileMap, seaMap := parse(input)
 
 	part1, part2 := 0, 0
-
-	borders := make(map[int][]*Rotation)
-	bordersPerTile := make(map[int][]*Rotation)
-	for id, tile := range tiles {
-		topLeft := tile.FindTopLeft()
-		bottomRight := tile.FindBottomRight()
-		topBorder, botBorder := 0, 0
-		shiftedTopBorder, shiftedBotBorder := 0, 0
-		shift := 0
-		for x := topLeft.X(); x <= bottomRight.X(); x++ {
-			if tile[twod.NewVector(x, topLeft.Y())] == '#' {
-				topBorder += 1 << shift
-				shiftedTopBorder += 1 << (9 - shift)
-			}
-			if tile[twod.NewVector(x, bottomRight.Y())] == '#' {
-				botBorder += 1 << shift
-				shiftedBotBorder += 1 << (9 - shift)
-			}
-			shift++
-		}
-		addBorder(borders, bordersPerTile, &Rotation{
-			ID:        id,
-			Value:     topBorder,
-			Direction: twod.UP,
-		})
-		addBorder(borders, bordersPerTile, &Rotation{
-			ID:        id,
-			Value:     shiftedTopBorder,
-			Direction: twod.UP,
-			Inverted:  true,
-		})
-		addBorder(borders, bordersPerTile, &Rotation{
-			ID:        id,
-			Value:     botBorder,
-			Direction: twod.DOWN,
-		})
-		addBorder(borders, bordersPerTile, &Rotation{
-			ID:        id,
-			Value:     shiftedBotBorder,
-			Direction: twod.DOWN,
-			Inverted:  true,
-		})
-
-		leftBorder, rightBorder := 0, 0
-		shiftedLeftBorder, shiftedRightBorder := 0, 0
-		shift = 0
-		for y := topLeft.Y(); y >= bottomRight.Y(); y-- {
-			if tile[twod.NewVector(topLeft.X(), y)] == '#' {
-				leftBorder += 1 << shift
-				shiftedLeftBorder += 1 << (9 - shift)
-			}
-			if tile[twod.NewVector(bottomRight.X(), y)] == '#' {
-				rightBorder += 1 << shift
-				shiftedRightBorder += 1 << (9 - shift)
-			}
-			shift++
-		}
-		addBorder(borders, bordersPerTile, &Rotation{
-			ID:        id,
-			Value:     leftBorder,
-			Direction: twod.LEFT,
-		})
-		addBorder(borders, bordersPerTile, &Rotation{
-			ID:        id,
-			Value:     shiftedLeftBorder,
-			Direction: twod.LEFT,
-			Inverted:  true,
-		})
-		addBorder(borders, bordersPerTile, &Rotation{
-			ID:        id,
-			Value:     rightBorder,
-			Direction: twod.RIGHT,
-		})
-		addBorder(borders, bordersPerTile, &Rotation{
-			ID:        id,
-			Value:     shiftedRightBorder,
-			Direction: twod.RIGHT,
-			Inverted:  true,
-		})
-	}
-
-	adjacent := make(map[int]int)
-	for _, rotations := range borders {
-		for _, rotation := range rotations {
-			if len(rotations) > 1 {
-				adjacent[rotation.ID]++
-			}
-		}
-	}
-
-	part1 = 1
-	for id, value := range adjacent {
-		if value == 4 {
-			part1 *= id
-		}
-	}
-
-	// part 2
-	bordersPerTileID := make(map[int]map[twod.Vector][]*Border)
-	arbitraryTile := -1
-	for id, tile := range tiles {
-		if arbitraryTile == -1 {
-			arbitraryTile = id
-			continue
-		}
-		bordersPerTileID[id] = make(map[twod.Vector][]*Border)
-		for i := 0; i < 8; i++ {
-			tile = tile.SetPositive()
-			tileBorders := findBorders(tile)
-			for dir, borders := range tileBorders {
-				for _, b := range borders {
-					b.Tile = tile
-					b.TileID = id
-				}
-				bordersPerTileID[id][dir] = append(bordersPerTileID[id][dir], borders...)
-			}
-			tile = tile.RotateRight() // try every rotation
-			if i%4 == 3 {
-				tile = tile.InvertY() // try every inversion
-			}
-		}
-	}
-
-	m := tiles[arbitraryTile]
-dance:
+buildingMap:
 	for len(bordersPerTileID) > 0 {
-		removeCouronne(m).Render()
-		actualBordersPerType := findBorders(m)
+		removeBorders(seaMap).Render()
+		actualBordersPerType := findBorders(seaMap)
 		for _, direction := range twod.FourDirections {
 			for id, potentialBordersPerDirection := range bordersPerTileID {
 				for _, potentialBorder := range potentialBordersPerDirection[direction] {
 					for _, actualBorder := range actualBordersPerType[direction.RotateDegree(180)] {
 						if actualBorder.Positions.Equal(potentialBorder.Positions) {
+							tileMap[actualBorder.BottomLeft] = id
 							for pos, char := range potentialBorder.Tile {
-								m[pos+actualBorder.BottomLeft] = char
+								seaMap[pos+actualBorder.BottomLeft] = char
 							}
 							delete(bordersPerTileID, id)
-							continue dance
+							continue buildingMap
 						}
 					}
 				}
 			}
 		}
 	}
+	part1 = tileMap[tileMap.FindTopLeft()].(int)
+	part1 *= tileMap[tileMap.FindTopRight()].(int)
+	part1 *= tileMap[tileMap.FindBottomRight()].(int)
+	part1 *= tileMap[tileMap.FindBottomLeft()].(int)
 
 	seaMonster := twod.NewMapFromInput(`                  # 
 #    ##    ##    ###
  #  #  #  #  #  #   `).Filter('#').SetPositive().Translate(-1i)
-	fmt.Println(seaMonster)
-	m = removeCouronne(m.Filter('#'))
-	fmt.Println(m)
+	seaMap = removeBorders(seaMap.Filter('#'))
 	for i := 0; i < 8; i++ {
-		m.Render()
+		seaMap.Render()
 		monsters := make(twod.Map)
-	dance2:
-		for k := range m {
+	lookingForSeaMonster:
+		for k := range seaMap {
 			potentialMonster := make(twod.Map)
 			for k2 := range seaMonster {
-				_, exist := m[k+k2]
+				_, exist := seaMap[k+k2]
 				if !exist {
-					continue dance2
+					continue lookingForSeaMonster
 				}
-				//potentialMonster.Render()
 				potentialMonster[k+k2] = 'O'
 			}
 			monsters = monsters.Merge(potentialMonster)
 			if len(potentialMonster) > 0 {
-				m.Merge(monsters).Render()
+				seaMap.Merge(monsters).Render()
 			}
 		}
 		if len(monsters) == 0 {
-			m = m.RotateRight() // try every rotation
+			seaMap = seaMap.RotateRight() // try every rotation
 			if i%4 == 3 {
-				m = m.InvertY() // try every inversion
+				seaMap = seaMap.InvertY() // try every inversion
 			}
 			continue
 		}
-		part2 = len(m) - len(monsters)
+		part2 = len(seaMap) - len(monsters)
 		break
 	}
 
 	return part1, part2
 }
 
-func removeCouronne(m twod.Map) twod.Map {
+func removeBorders(m twod.Map) twod.Map {
 	cleanedMap := make(twod.Map)
 	for pos, value := range m.SetPositive() {
 		x := pos.X()
@@ -227,6 +104,7 @@ type Border struct {
 	TileID int
 }
 
+// findBorders will return all borders sorted by side (UP,DOWN,LEFT,RIGHT).
 func findBorders(m twod.Map) map[twod.Vector][]*Border {
 	p := twod.NewPoint(m.FindBottomLeft(), twod.UP)
 	// find first pixel
@@ -241,7 +119,6 @@ func findBorders(m twod.Map) map[twod.Vector][]*Border {
 	// Follow borders
 	seen := make(map[twod.Vector]struct{})
 	borders := make(map[twod.Vector][]*Border)
-	p.Steps = 0
 	for {
 		if _, exist := seen[p.Pos]; exist {
 			break
@@ -291,44 +168,44 @@ func findBorders(m twod.Map) map[twod.Vector][]*Border {
 	return borders
 }
 
-//type Tile struct {
-//
-//	AllRotations map[twod.Vector]twod.Map
-//}
-
-func addBorder(borders, bordersPerTile map[int][]*Rotation, rotation *Rotation) {
-	_, exist := borders[rotation.Value]
-	if !exist {
-		borders[rotation.Value] = []*Rotation(nil)
-	}
-	_, exist = bordersPerTile[rotation.ID]
-	if !exist {
-		bordersPerTile[rotation.ID] = []*Rotation(nil)
-	}
-	borders[rotation.Value] = append(borders[rotation.Value], rotation)
-	bordersPerTile[rotation.ID] = append(bordersPerTile[rotation.ID], rotation)
-}
-
-type Rotation struct {
-	Value     int
-	ID        int
-	Direction twod.Vector
-	Inverted  bool
-}
-
-func parse(s string) map[int]twod.Map {
+func parse(s string) (map[int]map[twod.Vector][]*Border, twod.Map, twod.Map) {
 	maps := strings.Split(s, "\n\n")
-	m := make(map[int]twod.Map)
+
+	bordersPerTileID := make(map[int]map[twod.Vector][]*Border)
+	tileMap := make(twod.Map)
+	var seaMap twod.Map
 	for _, mapLines := range maps {
 		tmp := strings.Split(mapLines, "\n")
 		id := 0
 		pkg.MustScanf(tmp[0], "Tile %d:", &id)
-		m[id] = twod.NewMapFromInput(strings.Join(tmp[1:], "\n"))
+		tile := twod.NewMapFromInput(strings.Join(tmp[1:], "\n"))
+		if seaMap == nil {
+			tileMap[0] = id
+			seaMap = tile
+			continue
+		}
 
+		bordersPerTileID[id] = make(map[twod.Vector][]*Border)
+		for i := 0; i < 8; i++ {
+			tile = tile.SetPositive()
+			tileBorders := findBorders(tile)
+			for dir, borders := range tileBorders {
+				for _, b := range borders {
+					b.Tile = tile
+					b.TileID = id
+				}
+				bordersPerTileID[id][dir] = append(bordersPerTileID[id][dir], borders...)
+			}
+			tile = tile.RotateRight() // try every rotation
+			if i%4 == 3 {
+				tile = tile.InvertY() // try every inversion
+			}
+		}
 	}
-	return m
+
+	return bordersPerTileID, tileMap, seaMap
 }
 
 func main() {
-	execute.RunWithPixel(run, nil, puzzle, true)
+	execute.Run(run, tests, puzzle, true)
 }
